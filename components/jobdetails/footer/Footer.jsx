e67f2d2119ebe614a3d607a9609aab349590ad29
Nwobodo <nwobodokenechukwu2@gmail.com>
2023-04-24 17:31:08 +0100

    Implementing the like button

--- components/jobdetails/footer/Footer.jsx
+++ components/jobdetails/footer/Footer.jsx
@@ -1,12 +1,46 @@
+import {useState, useEffect, useCallback} from 'react';
 import {View, Text, TouchableOpacity, Image, Linking} from 'react-native';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from './footer.style';
 import {icons} from '../../../constants';
 
 const Footer = ({url}) => {
+  const [liked, setLiked] = useState (true);
+
+  // const storeLike = async liked => {
+  //   try {
+  //     const value = await AsyncStorage.setItem ('@storage_Key', liked);
+  //   } catch (e) {
+  //     console.log (e);
+  //   }
+  // };
+
+  // const getData = async () => {
+  //   try {
+  //     const value = await AsyncStorage.getItem ('@storage_Key');
+
+  //     return value;
+  //   } catch (e) {
+  //     // error reading value
+  //     console.log (e);
+  //   }
+  // };
+
+  // useEffect (
+  //   () => {
+  //     getData ();
+  //   },
+  //   [liked]
+  // );
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.likeBtn}>
+      <TouchableOpacity
+        style={liked ? styles.likeBtn : styles.likedBtn}
+        onPress={() => {
+          setLiked (!liked);
+        }}
+      >
         <Image
           source={icons.heartOutline}
           resizeMode="contain"
@@ -18,7 +52,9 @@ const Footer = ({url}) => {
         style={styles.applyBtn}
         onPress={() => Linking.openURL (url)}
       >
-        <Text style={styles.applyBtnText}>Apply for Job</Text>
+        <Text style={styles.applyBtnText}>
+          Apply for Job
+        </Text>
       </TouchableOpacity>
     </View>
   );
